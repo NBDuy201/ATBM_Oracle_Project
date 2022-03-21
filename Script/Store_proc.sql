1893beef78ff690dd32af9302bb0941b4ddd6a1e
PhamAnhTuan084 <tuansoi19127084@gmail.com>
2022-03-22 01:06:10 +0700

    Update Store_proc.sql

--- Script/Store_proc.sql
+++ Script/Store_proc.sql
@@ -134,3 +134,80 @@ begin
     end if;
 end;
 /
+
+---- CHUA TEST ------
+-- Cau 4 Cap quyen cho user
+CREATE OR REPLACE PROCEDURE GRANT_PRIVILEGES_USER(user_name IN NVARCHAR2, n_pri IN NVARCHAR2, n_col IN NVARCHAR2, n_tab IN NVARCHAR2, n_option IN NVARCHAR2)
+IS
+   	n_count INTEGER := 0;
+BEGIN
+    select count(*) into n_count from all_users where username = user_name;
+   	if n_count = 0 then
+        dbms_output.put_line('User does not exist');
+        return;
+   	end if;
+    
+    if  n_pri = 'UPDATE' then
+        if n_option = 'TRUE' then
+            EXECUTE IMMEDIATE ('grant ' || n_pri || '(' || n_col || ') ' || ' on sys.' || n_tab || ' to ' || user_name || ' with grant option');
+        else
+            EXECUTE IMMEDIATE ('grant ' || n_pri || '(' || n_col || ') ' || ' on sys.' || n_tab || ' to ' || user_name);
+        end if;
+    else
+        if n_option = 'TRUE' then
+            EXECUTE IMMEDIATE ('grant ' || n_pri || ' on sys.' || n_tab || ' to ' || user_name || ' with grant option');
+        else
+            EXECUTE IMMEDIATE ('grant ' || n_pri || ' on sys.' || n_tab || ' to ' || user_name);
+        end if; 
+    end if;
+END  GRANT_PRIVILEGES_USER;
+/
+
+-- Cap quyen he thong cho user:
+CREATE OR REPLACE PROCEDURE GRANT_PRIVILEGES_SYS_USER(user_name IN NVARCHAR2, n_pri IN NVARCHAR2, n_option IN NVARCHAR2)
+IS
+   	n_count INTEGER := 0;
+BEGIN
+    select count(*) into n_count from all_users where username = user_name;
+   	if n_count = 0 then
+        dbms_output.put_line('User does not exist');
+        return;
+   	end if;
+    
+     if n_option = 'TRUE' then
+        EXECUTE IMMEDIATE ('grant ' || n_pri || ' to ' || user_name || ' with grant option');
+    else
+        EXECUTE IMMEDIATE ('grant ' || n_pri || ' to ' || user_name);
+    end if;
+END  GRANT_PRIVILEGES_SYS_USER;
+/
+
+CREATE OR REPLACE PROCEDURE GRANT_PRIVILEGES_ROLE(role_name IN NVARCHAR2, n_pri IN NVARCHAR2, n_col IN NVARCHAR2, n_tab IN NVARCHAR2)
+IS
+BEGIN
+    
+    if  n_pri = 'UPDATE' then
+        EXECUTE IMMEDIATE ('grant ' || n_pri || ' (' || n_col || ') ' || ' on sys.' || n_tab || ' to ' || role_name);
+    else
+        EXECUTE IMMEDIATE ('grant ' || n_pri || ' on sys.' || n_tab || ' to ' || role_name);
+    end if;
+END  GRANT_PRIVILEGES_ROLE;
+/
+
+CREATE OR REPLACE PROCEDURE GRANT_ROLE_TO_USER(role_name IN NVARCHAR2, user_name IN NVARCHAR2, n_option IN NVARCHAR2)
+IS
+   	n_count INTEGER := 0;
+BEGIN
+    select count(*) into n_count from all_users where username = user_name;
+   	if n_count = 0 then
+        dbms_output.put_line('User does not exist');
+        return;
+   	end if; 
+    
+    if n_option = 'TRUE' then
+        EXECUTE IMMEDIATE ('grant ' || role_name || ' to ' || user_name || ' with admin option');
+    else
+        EXECUTE IMMEDIATE ('grant ' || role_name || ' to ' || user_name);
+    end if;
+END GRANT_ROLE_TO_USER;
+/
