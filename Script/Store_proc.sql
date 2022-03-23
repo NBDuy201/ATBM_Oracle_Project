8bbd9ac276d0b3dd7b606dcb89660e283f24d701
PhamAnhTuan084 <tuansoi19127084@gmail.com>
2022-03-23 10:01:42 +0700

    cau 4

--- Script/Store_proc.sql
+++ Script/Store_proc.sql
@@ -107,3 +107,112 @@ BEGIN
     EXECUTE IMMEDIATE (temp_query);
 END ;
 /
+
+-Cau 3 Doi password user:
+create or replace procedure Alter_User(User_name in varchar2,Pass_Word in varchar2)
+authid current_user
+is
+    Tmp_count int;
+    Tmp_query varchar2(100);
+begin
+    select count(*) into Tmp_count from all_users where username=User_name;
+    if(Tmp_count!=0)then
+    Tmp_query :='ALTER USER '|| User_name||' IDENTIFIED BY '||Pass_Word;
+    execute immediate(Tmp_query);
+    else
+    RAISE_APPLICATION_ERROR(-20000, 'User da ton tai');
+    end if;
+end;
+/
+
+-- Cau 3 Hieu chinh role
+create or replace procedure Alter_Role (Role_name in varchar2, Pass_Word in varchar2)
+authid current_user
+is
+    Tmp_count int;
+    Tmp_query varchar2(100);
+begin
+    if(Pass_Word=' ') then
+    Tmp_query := 'ALTER ROLE '|| Role_Name|| ' Not IDENTIFIED';
+    execute immediate(Tmp_query);
+    elsif(pass_word!=' ') then
+    Tmp_query := 'ALTER ROLE '|| Role_Name|| ' IDENTIFIED BY'|| Pass_Word;
+    end if;
+end;
+/
+
+---- CHUA TEST ------
+-- Cau 4 Cap quyen cho user
+CREATE OR REPLACE PROCEDURE GRANT_PRIVILEGES_USER(user_name IN NVARCHAR2, n_pri IN NVARCHAR2, n_col IN NVARCHAR2, n_tab IN NVARCHAR2, n_option IN NVARCHAR2)
+IS
+   	n_count INTEGER := 0;
+BEGIN
+    select count(*) into n_count from all_users where username = user_name;
+   	if n_count = 0 then
+        dbms_output.put_line('User does not exist');
+        return;
+   	end if;
+
+    if  n_pri = 'UPDATE' then
+        if n_option = 'TRUE' then
+            EXECUTE IMMEDIATE ('grant ' || n_pri || '(' || n_col || ') ' || ' on sys.' || n_tab || ' to ' || user_name || ' with grant option');
+        else
+            EXECUTE IMMEDIATE ('grant ' || n_pri || '(' || n_col || ') ' || ' on sys.' || n_tab || ' to ' || user_name);
+        end if;
+    else
+        if n_option = 'TRUE' then
+            EXECUTE IMMEDIATE ('grant ' || n_pri || ' on sys.' || n_tab || ' to ' || user_name || ' with grant option');
+        else
+            EXECUTE IMMEDIATE ('grant ' || n_pri || ' on sys.' || n_tab || ' to ' || user_name);
+        end if; 
+    end if;
+END  GRANT_PRIVILEGES_USER;
+/
+
+-- Cap quyen he thong cho user:
+CREATE OR REPLACE PROCEDURE GRANT_PRIVILEGES_SYS_USER(user_name IN NVARCHAR2, n_pri IN NVARCHAR2, n_option IN NVARCHAR2)
+IS
+   	n_count INTEGER := 0;
+BEGIN
+    select count(*) into n_count from all_users where username = user_name;
+   	if n_count = 0 then
+        dbms_output.put_line('User does not exist');
+        return;
+   	end if;
+
+     if n_option = 'TRUE' then
+        EXECUTE IMMEDIATE ('grant ' || n_pri || ' to ' || user_name || ' with grant option');
+    else
+        EXECUTE IMMEDIATE ('grant ' || n_pri || ' to ' || user_name);
+    end if;
+END  GRANT_PRIVILEGES_SYS_USER;
+/
+
+CREATE OR REPLACE PROCEDURE GRANT_PRIVILEGES_ROLE(role_name IN NVARCHAR2, n_pri IN NVARCHAR2, n_col IN NVARCHAR2, n_tab IN NVARCHAR2)
+IS
+BEGIN
+
+    if  n_pri = 'UPDATE' then
+        EXECUTE IMMEDIATE ('grant ' || n_pri || ' (' || n_col || ') ' || ' on sys.' || n_tab || ' to ' || role_name);
+    else
+        EXECUTE IMMEDIATE ('grant ' || n_pri || ' on sys.' || n_tab || ' to ' || role_name);
+    end if;
+END  GRANT_PRIVILEGES_ROLE;
+/
+
+CREATE OR REPLACE PROCEDURE GRANT_ROLE_TO_USER(role_name IN NVARCHAR2, user_name IN NVARCHAR2, n_option IN NVARCHAR2)
+IS
+   	n_count INTEGER := 0;
+BEGIN
+    select count(*) into n_count from all_users where username = user_name;
+   	if n_count = 0 then
+        dbms_output.put_line('User does not exist');
+        return;
+   	end if; 
+
+    if n_option = 'TRUE' then
+        EXECUTE IMMEDIATE ('grant ' || role_name || ' to ' || user_name || ' with admin option');
+    else
+        EXECUTE IMMEDIATE ('grant ' || role_name || ' to ' || user_name);
+    end if;
+END GRANT_ROLE_TO_USER;
