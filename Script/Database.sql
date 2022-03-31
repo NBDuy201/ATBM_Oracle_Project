b414783d733f5056e0a845378f1a950a109ce797
PhamAnhTuan084 <tuansoi19127084@gmail.com>
2022-03-31 15:15:40 +0700

    BỎ DẤU

--- Script/Database.sql
+++ Script/Database.sql
@@ -1,81 +1,76 @@
-CREATE TABLE "CSYT" 
-(
-  "MÃCSYT" varchar2(30),
-  "TÊNCSYT" NVARCHAR2(50),
-  "ĐCCSYT" Nvarchar2(250),
-  "SĐTCSYT" varchar(12),
-  PRIMARY KEY ("MÃCSYT")
+CREATE TABLE "CSYT" (
+  "MACSYT" varchar2(30),
+  "TENCSYT" NVARCHAR2(50),
+  "DCCSYT" Nvarchar2(250),
+  "SDTCSYT" varchar(12),
+  PRIMARY KEY ("MACSYT")
 );
 
-CREATE TABLE "BỆNHNHÂN" 
-(
-  "MÃBN" varchar2(30),
-  "MÃCSYT" varchar2(30),
-  "TÊNBN" NVARCHAR2(50),
+CREATE TABLE "BENHNHAN" (
+  "MABN" varchar2(30),
+  "MACSYT" varchar2(30),
+  "TENBN" NVARCHAR2(50),
   "CMND" Number(12),
-  "NGÀYSINH" date,
-  "SỐNHÀ" Number(5),
-  "TÊNĐƯỜNG" nvarchar2(50),
-  "QUẬNHUYỆN" nvarchar2(50),
-  "TỈNHTP" nvarchar2(50),
-  "TIỀNSỬBỆNH" nvarchar2(250),
-  "TIỀNSỬBỆNHGĐ" nvarchar2(250),
-  "DỊỨNGTHUỐC" nvarchar2(250),
-  PRIMARY KEY ("MÃBN"),
-  CONSTRAINT "FK_BỆNHNHÂN.MÃCSYT"
-    FOREIGN KEY ("MÃCSYT")
-      REFERENCES "CSYT"("MÃCSYT")
+  "NGAYSINH" date,
+  "SONHA" Number(5),
+  "TENĐUONG" nvarchar2(50),
+  "QUANHUYEN" nvarchar2(50),
+  "TINHTP" nvarchar2(50),
+  "TIENSUBENH" nvarchar2(250),
+  "TIENSUBENHGD" nvarchar2(250),
+  "DIUNGTHUOC" nvarchar2(250),
+  PRIMARY KEY ("MABN"),
+  CONSTRAINT "FK_BENHNHAN.MACSYT"
+    FOREIGN KEY ("MACSYT")
+      REFERENCES "CSYT"("MACSYT")
 );
 
-CREATE TABLE "NHÂNVIÊN" 
-(
-  "MÃNV" varchar2(30),
-  "HỌTÊN" NVARCHAR2(50),
-  "PHÁI" nvarchar2(20),
-  "NGÀYSINH" date,
+CREATE TABLE "NHANVIEN" (
+  "MANV" varchar2(30),
+  "HOTEN" NVARCHAR2(50),
+  "PHAI" nvarchar2(20),
+  "NGAYSINH" date,
   "CMND" Number(12),
-  "QUÊQUÁN" Nvarchar2(50),
-  "SỐĐT" varchar(12),
+  "QUEQUAN" Nvarchar2(50),
+  "SODT" varchar(12),
   "CSYT" varchar2(30),
-  "VAITRÒ" nvarchar2(50),
-  "CHUYÊNKHOA" nvarchar2(50),
-  PRIMARY KEY ("MÃNV"),
-  CONSTRAINT "FK_NHÂNVIÊN.CSYT"
+  "VAITRO" nvarchar2(50),
+  "CHUYENKHOA" nvarchar2(50),
+  PRIMARY KEY ("MANV"),
+  CONSTRAINT "FK_NHANVIEN.CSYT"
     FOREIGN KEY ("CSYT")
-      REFERENCES "CSYT"("MÃCSYT")
+      REFERENCES "CSYT"("MACSYT")
 );
 
-CREATE TABLE "HSBA" 
-(
-  "MÃHSBA" varchar2(30),
-  "MÃBN" varchar2(30),
-  "NGÀY" date,
-  "CHẨNĐOÁN" nvarchar2(250),
-  "MÃBS" varchar2(30),
-  "MÃKHOA" varchar2(30),
-  "MÃCSYT" varchar2(30),
-  "KẾTLUẬN" nvarchar2(250),
-  PRIMARY KEY ("MÃHSBA"),
-  CONSTRAINT "FK_HSBA.MÃBN"
-    FOREIGN KEY ("MÃBN")
-      REFERENCES "BỆNHNHÂN"("MÃBN"),
-  CONSTRAINT "FK_HSBA.MÃCSYT"
-    FOREIGN KEY ("MÃCSYT")
-      REFERENCES "CSYT"("MÃCSYT")
+CREATE TABLE "HSBA" (
+  "MAHSBA" varchar2(30),
+  "MABN" varchar2(30),
+  "NGAY" date,
+  "CHANĐOAN" nvarchar2(250),
+  "MABS" varchar2(30),
+  "MAKHOA" varchar2(30),
+  "MACSYT" varchar2(30),
+  "KETLUAN" nvarchar2(250),
+  PRIMARY KEY ("MAHSBA"),
+  CONSTRAINT "FK_HSBA.MABN"
+    FOREIGN KEY ("MABN")
+      REFERENCES "BENHNHAN"("MABN"),
+  CONSTRAINT "FK_HSBA.MACSYT"
+    FOREIGN KEY ("MACSYT")
+      REFERENCES "CSYT"("MACSYT")
 );
 
-CREATE TABLE "HSBA_DV" 
-(
-  "MÃHSBA" varchar2(30),
-  "MÃDV" varchar2(30),
-  "NGÀY" date,
-  "MÃKTV" varchar2(30),
-  "KẾTQUẢ" nvarchar2(250),
-  PRIMARY KEY ("MÃHSBA", "MÃDV", "NGÀY"),
-  CONSTRAINT "FK_HSBA_DV.MÃKTV"
-    FOREIGN KEY ("MÃKTV")
-      REFERENCES "NHÂNVIÊN"("MÃNV"),
-  CONSTRAINT "FK_HSBA_DV.MÃHSBA"
-    FOREIGN KEY ("MÃHSBA")
-      REFERENCES "HSBA"("MÃHSBA")
+CREATE TABLE "HSBA_DV" (
+  "MAHSBA" varchar2(30),
+  "MADV" varchar2(30),
+  "NGAY" date,
+  "MAKTV" varchar2(30),
+  "KETQUA" nvarchar2(250),
+  PRIMARY KEY ("MAHSBA", "MADV", "NGAY"),
+  CONSTRAINT "FK_HSBA_DV.MAKTV"
+    FOREIGN KEY ("MAKTV")
+      REFERENCES "NHANVIEN"("MANV"),
+  CONSTRAINT "FK_HSBA_DV.MAHSBA"
+    FOREIGN KEY ("MAHSBA")
+      REFERENCES "HSBA"("MAHSBA")
 );
