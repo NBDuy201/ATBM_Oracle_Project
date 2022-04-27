4b5962a6004c2c3c9103dc570b2331a35daadcaa
NBDuy201 <85396503+NBDuy201@users.noreply.github.com>
2022-04-27 15:56:32 +0700

    Create + Delete user procedure fix

--- Script/Store_Proc_DBA.sql
+++ Script/Store_Proc_DBA.sql
@@ -32,41 +32,73 @@ END view_allPrivi;
 
 -- Cau 3 --
 -- create User
-create or replace procedure Grant_NewUser(User_name in varchar2,Pass_Word in varchar2)
+create or replace procedure Grant_NewUser(User_name in varchar2, Pass_Word in varchar2, vaitro in NVARCHAR2, CoSoYTe in VARCHAR2)
 authid current_user 
 is
     Tmp_count int;
     Tmp_query varchar2(100);
 Begin
-    select count(*) into Tmp_count from all_users where username=User_name;
-    if(Tmp_count!=0)
-    then 
-    RAISE_APPLICATION_ERROR(-20000,'User da ton tai');
+    select count(*) into Tmp_count from all_users where username=upper(User_name);
+    if(Tmp_count!=0) then 
+        RAISE_APPLICATION_ERROR(-20000,'User da ton tai');
     end if;
+    
+    -- Create User
     Tmp_query:='Create user '|| User_name||' identified by '||Pass_Word;
+    --DBMS_OUTPUT.PUT_LINE(Tmp_query);
     execute immediate(Tmp_query);
-    Tmp_query:='grant create session to'|| User_name;
+    
+    Tmp_query:='grant create session to '|| User_name;
+    --DBMS_OUTPUT.PUT_LINE(Tmp_query);
+    execute immediate(Tmp_query);
+         
+     -- Insert User in table
+    if(vaitro = N'Bệnh Nhân') then
+        Tmp_query:='insert into BENHNHAN(MABN, MACSYT) Values(:ma, :cs)';
+         execute immediate(Tmp_query)
+            using User_name, CoSoYTe;
+     else
+        Tmp_query:='insert into NHANVIEN(MANV, CSYT, VAITRO) Values(:ma, :cs, :vt)';
+        execute immediate(Tmp_query)
+            using User_name, CoSoYTe, vaitro;
+    End if;
 End;
 /
+--exec Grant_NewUser('Test', 'Test', N'Bệnh Nhân', 'CS0');
 
 -- Delete User
-create or replace procedure Drop_User (User_Name in varchar2)
+create or replace procedure Drop_User (User_Name in varchar2, vaitro in NVARCHAR2)
 authid current_user is
     Tmp_query varchar(100);
     Tmp_count int;
+    Tmp_count2 int;
 Begin
-    select count(*) into Tmp_count from all_users where username=User_name;
-    if(Tmp_count!=0) then
+    select count(*) into Tmp_count from all_users where username=upper(User_name);
+    if(Tmp_count!=0) then        
+        -- Check BN or NV
+        select count(*) into Tmp_count from BENHNHAN where MABN = User_name;
+        select count(*) into Tmp_count2 from NHANVIEN where MANV = User_name;
+        
+        if(vaitro = N'Bệnh Nhân' and Tmp_count != 0) then
+            Tmp_query:='Delete From BENHNHAN where MABN = :ma';
+        elsif(vaitro != N'Bệnh Nhân' and Tmp_count2 != 0) then
+            Tmp_query:='Delete From NHANVIEN where MANV = :ma';
+        else
+            RAISE_APPLICATION_ERROR(-20000, N'User không đúng vai trò');
+        end if;
+        -- Exec
+        -- Delete from table
+        execute immediate(Tmp_query)
+                using User_name;
+        -- Drop user       
         Tmp_query:='Drop user '||User_Name; 
         execute IMMEDIATE (Tmp_query);
     else 
-        RAISE_APPLICATION_ERROR(-20000,'User chua ton tai');
+        RAISE_APPLICATION_ERROR(-20000, N'User chưa tồn tại');
     end if;
 End;
 /
-
---exec Drop_User('TEST');
---Grant create session to TEST;
+--exec Drop_User('Test', N'Bệnh Nhân');
 
 CREATE OR REPLACE PROCEDURE view_roles
 (
