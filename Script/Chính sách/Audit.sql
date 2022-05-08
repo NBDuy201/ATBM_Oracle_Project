f259bb4566586943d0c7bf6d55aa2a6adaee3047
PhamAnhTuan084 <tuansoi19127084@gmail.com>
2022-05-08 10:10:47 +0700

    FIX CHO HOP LY

--- Script/Chính sách/Audit.sql
+++ Script/Chính sách/Audit.sql
@@ -1,8 +1,9 @@
 -- CHECK POLICY DA DUOC CAU HINH
 select * from dba_audit_policies;
 
-show parameter audit;
-
+-- KIEM TRA LICH SU
+select* from  dba_fga_audit_trail
+WHERE TIMESTAMP > SYSDATE-1 ORDER BY timestamp DESC;
 
 -- sql command 
 ---Thiet lap audit voi tham so db,extended 
@@ -12,67 +13,21 @@ startup
 alter system set audit_trail=db,extended scope=spfile; -- disable audit
 
 -- AUDIT BINH THUONG 
-audit select,insert,update, delete on SINHVIEN by access;
+audit insert CSYT by access;
 
 --- CAI DAT FGA AUDIT
 BEGIN
   DBMS_FGA.ADD_POLICY(
-   object_schema      => 'DBA_BV',
+   object_schema      => 'TUAN',
    object_name        => 'CSYT',
    policy_name        => 'Track_CSYT',
-   statement_types    => 'INSERT, UPDATE, DELETE'
+   statement_types    => 'INSERT, UPDATE'
    );
 END;
 /
 
-BEGIN
-  DBMS_FGA.ADD_POLICY(
-   object_schema      => 'DBA_BV',
-   object_name        => 'BENHNHAN',
-   policy_name        => 'Track_BENHNHAN',
-   statement_types    => 'SELECT, INSERT, UPDATE, DELETE'
-   );
-END;
-/
+--- TEST
+SELECT * FROM CSYT;
+Insert into CSYT (MACSYT,TENCSYT,DCCSYT,SDTCSYT) values ('CS22','IOL Group','DeLTA','721-5190');
 
-BEGIN
-  DBMS_FGA.ADD_POLICY(
-   object_schema      => 'DBA_BV',
-   object_name        => 'NHANVIEN',
-   policy_name        => 'Track_NHANVIEN',
-   statement_types    => 'SELECT, INSERT, UPDATE, DELETE'
-   );
-END;
-/
-
-BEGIN
-  DBMS_FGA.ADD_POLICY(
-   object_schema      => 'DBA_BV',
-   object_name        => 'HSBA',
-   policy_name        => 'Track_HSBA',
-   statement_types    => 'SELECT, INSERT, UPDATE, DELETE'
-   );
-END;
-/
-BEGIN
-  DBMS_FGA.ADD_POLICY(
-   object_schema      => 'DBA_BV',
-   object_name        => 'HSBA_DV',
-   policy_name        => 'Track_HSBA_DV',
-   statement_types    => 'SELECT, INSERT, UPDATE, DELETE'
-   );
-END;
-/
-
--- TEST
-select * from NHANVIEN;
-Insert into NHANVIEN (MANV,HOTEN,PHAI,NGAYSINH,CMND,QUEQUAN,SODT,CSYT,VAITRO,CHUYENKHOA) values ('NV20','LoDWm','Male',to_date('01-JAN-90','DD-MON-RR'),'ID010','Leon','722-6220','CS3','Thanh tra','');
-
--- KIEM TRA LICH SU
-select* from  dba_fga_audit_trail
-WHERE TIMESTAMP > SYSDATE-1 ORDER BY timestamp DESC;
-
-select * from SYS.fga_log$ WHERE NTIMESTAMP# > SYSDATE-1 ORDER BY ntimestamp# DESC;
-
-
-
+DELETE FROM CSYT WHERE macsyt = 'CS22';
